cc4fd32f313af8214e17f5a129ca98ee52c7c329
Gonzalo Uceda Castro <gon_u@hotmail.com>
2020-12-29 12:35:29 +0100

    divider:: Divider component - move divider from frontEnd - add legend use case to component

---
+++ src/components/divider/divider.styles.ts
@@ -0,0 +1,5 @@
+import { SxStyleProp } from 'rebass';
+
+export default {
+  borderBottom: '2px solid #F5F5F5',
+} as SxStyleProp;
---
+++ src/components/divider/index.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react';
+import { Box, BoxProps } from 'rebass';
+import Microlabeling from '../typography/microlabeling';
+
+import styles from './divider.styles';
+
+export interface DividerProps extends Omit<BoxProps, 'css'> {
+  mt?: string;
+  mb?: string;
+  mr?: string;
+  ml?: string;
+  width?: string;
+  legend?: string;
+}
+
+const Divider: FC<DividerProps> = ({
+  mt = '20px',
+  mb = '20px',
+  mr = '0',
+  ml,
+  width,
+  legend,
+  ...props
+}) => (
+  <Box
+    mt={mt}
+    mb={mb}
+    mr={mr}
+    ml={ml || (legend ? '0' : '-20px')}
+    width={width || (legend ? '100%' : 'calc(100% + 40px)')}
+    {...props}
+  >
+    {legend ? <Microlabeling gray>{legend}</Microlabeling> : null}
+    <Box mt={legend ? '5px' : '0'} sx={styles} height="1px" />
+  </Box>
+);
+
+export default Divider;
