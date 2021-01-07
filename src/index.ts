08bee960f6c2ed6145015ec8f526dcafc0c5a29e
Gonzalo Uceda <gon_u@hotmail.com>
2021-01-07 17:55:57 +0100

    Divider component migration and legend & Select error status   (#67)
    * divider:: Divider component
    - move divider from frontEnd
    - add legend use case to component
    
    * Select.tsx::
    - Allow error status
    - Allow info message
    
    * divider:: use color labels

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,7 @@ import SplitGraph from './components/graphs/split-graph';
 import ToggleButton from './components/toggle-button';
 import NotificationsContainer from './components/notifications/index';
 import NotificationsManager from './components/notifications/notifications-manager';
+import Divider from './components/divider';
 
 // Popups
 import Drawer from './components/popup/drawer';
@@ -131,6 +132,7 @@ export {
   TooltipPositions,
   Radio,
   RadioGroup,
+  Divider,
   // Popups
   usePopup,
   Popup,
