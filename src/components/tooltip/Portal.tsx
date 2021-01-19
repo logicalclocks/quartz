cf0e84147b34ee0f197e4b76fdeb8ee5543b135e
YevgenChop <yevgenchop@gmail.com>
2021-01-19 13:33:17 +0200

    fix: - Minor fixes

---
+++ src/components/tooltip/Portal.tsx
@@ -0,0 +1,25 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+class Portal extends React.PureComponent {
+  private readonly el: HTMLDivElement;
+
+  constructor(props: any) {
+    super(props);
+    this.el = document.createElement('div');
+  }
+
+  componentDidMount() {
+    document.body.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    document.body.removeChild(this.el);
+  }
+
+  render() {
+    return ReactDOM.createPortal(this.props.children, this.el);
+  }
+}
+
+export default Portal;
