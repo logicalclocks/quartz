a23bb56c15626dd4c6e41fd851ff9ba6f0c7e4ea
yevgenchop@gmail.com <yevgenchop@gmail.com>
2021-03-16 11:44:29 +0200

    feat: - Updated code snippet

--- src/components/code/code.styles.ts
+++ src/components/code/code.styles.ts
@@ -1,14 +1,57 @@
 import { SxStyleProp } from 'rebass';
 
 export const boxStyles = {
+  width: '100%',
   lineHeight: '16px',
   overflowX: 'auto',
   whiteSpace: 'pre-wrap',
   wordBreak: 'break-all',
+  backgroundColor: 'grayShade3',
+  fonts: 'value',
+  fontWeight: 'text',
+  marginTop: 0,
+  marginBottom: 0,
+  paddingLeft: 20,
+};
+
+export const codeHeaderStyles = {
+  backgroundColor: 'grayShade2',
+  color: 'gray',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+};
+
+export const buttonsStyles = {
+  display: 'flex',
+  color: 'black',
+  fontFamily: 'Inter',
+  fontWeight: 700,
+  fontSize: '12px',
+  paddingTop: '2px',
+  paddingBottom: '3px',
+  paddingLeft: '8px',
+  paddingRight: '8px',
+  backgroundColor: 'grayShade2',
+  ':hover': {
+    backgroundColor: 'grayShade1',
+  },
+  ':active': {
+    backgroundColor: 'primaryShade1',
+  },
+  ':disabled': {
+    backgroundColor: 'primaryShade2',
+  },
+  borderLeftStyle: 'solid',
+  borderLeftWidth: 1,
+  borderLeftColor: 'grayShade1',
+  maxHeight: 23,
+  borderRadius: 0,
+  transition: 'all 0.8s cubic-bezier(0.19, 1, 0.22, 1)',
 };
 
 export default {
   p: '20px',
   justifyContent: 'space-between',
   alignItems: 'center',
+  flexDirection: 'column',
 } as SxStyleProp;
