c9092c7c91fa3da7b26310fdf9a0e975770846b7
sarah <mouffoksarah1@gmail.com>
2021-08-04 17:52:28 +0200

    gravatar to user initials

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -1,41 +1,51 @@
 import React, { FC } from 'react';
+import { Flex } from 'rebass';
 import Tooltip from '../tooltip';
-import { Box, Flex } from 'rebass';
 import styles from '../avatar/avatar.styles';
-import defaultPhoto from '../avatar/default.svg';
 import Label from '../label';
 
 export interface UserProps {
-  photo: string;
-  name: string;
+  firstName: string;
+  lastName?: string;
   title?: string;
   secondaryText?: string;
   isTooltipActive?: boolean;
 }
 
 const User: FC<UserProps> = ({
-  photo,
-  name,
+  firstName,
+  lastName,
   title,
   secondaryText,
   isTooltipActive = true,
 }: UserProps) => {
-  const capitalisedName = name.charAt(0).toLocaleUpperCase() + name.slice(1);
+  const capitalisedName = `
+  ${firstName ? firstName.charAt(0).toLocaleUpperCase() : ''}
+  ${lastName ? lastName.charAt(0).toLocaleUpperCase() : ''}
+  `;
+
+  const hue = Math.round(((capitalisedName.charCodeAt(0) - 64) * 360) / 26);
+  const color = (lightness: number) => `hsl(${hue}, 75%, ${lightness}%)`;
 
   const ImgBadge = () => (
-    <Box
-      as="img"
-      src={photo || defaultPhoto}
+    <Flex
       sx={{
         ...styles,
-        borderColor: `hsl(${Math.round(
-          ((capitalisedName.charCodeAt(0) - 64) * 360) / 26,
-        )}, 75%, 55%)`,
+        borderColor: color(55),
+        color: color(55),
+        fontWeight: 'bold',
+        fontFamily: 'Inter',
+        backgroundColor: color(90),
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '12px',
       }}
       minWidth="32px"
       height="32px"
       alt="User avatar"
-    />
+    >
+      {capitalisedName}
+    </Flex>
   );
 
   return (
