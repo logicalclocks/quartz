2b53d44b757c31add64c8b13d0e83fee16a5fcc7
Gonzalo Uceda Castro <gon_u@hotmail.com>
2021-08-25 08:33:42 +0200

    Reuse FormSummaryContainer in StickySummary

--- src/components/sticky-summary/index.tsx
+++ src/components/sticky-summary/index.tsx
@@ -1,21 +1,19 @@
 import React, { FC } from 'react';
-import { Box, Flex } from 'rebass';
-import styles, {
-  boxStyles,
-  boxButtonStyles,
-  mainBoxStyles,
-} from './sticky-summary.styles';
+import { Box } from 'rebass';
+import { boxStyles, boxButtonStyles } from './sticky-summary.styles';
 import Title from '../typography/title';
 import Labeling from '../typography/labeling';
 import Value from '../typography/value';
+import FormSummaryContainer, {
+  FormSummaryContainerProps,
+} from '../form-summary-container';
 
-export interface StickySummaryProps {
+export interface StickySummaryProps extends FormSummaryContainerProps {
   title?: string;
   firstValue?: string;
   secondValue?: string;
   mainButton: React.ReactNode;
   secondaryButton?: React.ReactNode;
-  hasScrollOnScreen?: boolean;
   errorsValue?: string;
 }
 
@@ -29,28 +27,26 @@ const StickySummary: FC<StickySummaryProps> = ({
   errorsValue,
   ...props
 }: StickySummaryProps) => (
-  <Flex sx={{ ...styles(hasScrollOnScreen) }} {...props}>
-    <Flex sx={{ ...mainBoxStyles }}>
-      <Box sx={{ ...boxStyles }}>
-        {title && <Title sx={{ textTransform: 'normal' }}>{title}</Title>}
-        {errorsValue && <Value color="labels.red">{errorsValue}</Value>}
-        {firstValue && !errorsValue && (
-          <Labeling gray bold>
-            {firstValue}
-          </Labeling>
-        )}
-        {secondValue && !errorsValue && (
-          <Labeling gray bold>
-            {secondValue}
-          </Labeling>
-        )}
-      </Box>
-      <Box sx={{ ...boxButtonStyles }}>
-        {secondaryButton && <Box>{secondaryButton}</Box>}
-        <Box>{mainButton}</Box>
-      </Box>
-    </Flex>
-  </Flex>
+  <FormSummaryContainer hasScrollOnScreen={hasScrollOnScreen} {...props}>
+    <Box sx={{ ...boxStyles }}>
+      {title && <Title sx={{ textTransform: 'normal' }}>{title}</Title>}
+      {errorsValue && <Value color="labels.red">{errorsValue}</Value>}
+      {firstValue && !errorsValue && (
+        <Labeling gray bold>
+          {firstValue}
+        </Labeling>
+      )}
+      {secondValue && !errorsValue && (
+        <Labeling gray bold>
+          {secondValue}
+        </Labeling>
+      )}
+    </Box>
+    <Box sx={{ ...boxButtonStyles }}>
+      {secondaryButton && <Box>{secondaryButton}</Box>}
+      <Box>{mainButton}</Box>
+    </Box>
+  </FormSummaryContainer>
 );
 
 export default StickySummary;
