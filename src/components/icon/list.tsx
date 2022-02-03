5f393ce17ae17a3321a2f34cde56f1c17cd1ab2a
giardiv <giardina.v@gmail.com>
2022-02-03 19:09:19 +0100

    update latest icons

--- src/components/icon/list.tsx
+++ src/components/icon/list.tsx
@@ -1100,8 +1100,8 @@ export function getIcon(
     case IconName.time:
       return (
         <svg
-          width="24"
-          height="24"
+          width={size}
+          height={size}
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -1123,8 +1123,8 @@ export function getIcon(
     case IconName.commit:
       return (
         <svg
-          width="24"
-          height="24"
+          width={size}
+          height={size}
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -1140,8 +1140,8 @@ export function getIcon(
     case IconName.block:
       return (
         <svg
-          width="24"
-          height="24"
+          width={size}
+          height={size}
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
