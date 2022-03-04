fc309cd58f9b85e7b5e4c26ae67e6f0dad25efe5
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2022-03-04 12:07:00 +0100

    Add more details of ProgressBar2 in story

--- src/components/bar/progress-bar-2/progress-bar-2.stories.tsx
+++ src/components/bar/progress-bar-2/progress-bar-2.stories.tsx
@@ -1,45 +1,91 @@
-import { Box } from 'rebass';
-import { Story } from '@storybook/react/types-6-0';
+import { Box, Button, Flex } from 'rebass';
+import { Meta, Story } from '@storybook/react/types-6-0';
 import ProgressBar2C, { ProgressBar2Props } from '.';
+import { useState } from 'react';
 
-export const ProgressBar2: Story<ProgressBar2Props> = (props) => (
-  <Box width="700px">
-    <ProgressBar2C {...props} />
-  </Box>
-);
+export default {
+  title: 'Quartz/ProgressBar2',
+  component: ProgressBar2C,
+  decorators: [
+    (Story) => (
+      <Box width="700px">
+        <Story />
+      </Box>
+    ),
+  ],
+} as Meta;
 
-ProgressBar2.argTypes = {
+export const Default: Story<ProgressBar2Props> = (args) => {
+  const [isAnimating, setIsAnimating] = useState<boolean>();
+  return (
+    <>
+      <Flex my="20px" justifyContent="center" sx={{ gap: '20px' }}>
+        <Button disabled={isAnimating} onClick={() => setIsAnimating(true)}>
+          {isAnimating === false ? 'restart' : 'start'}
+        </Button>
+        <Button disabled={!isAnimating} onClick={() => setIsAnimating(false)}>
+          done
+        </Button>
+      </Flex>
+      <ProgressBar2C {...args} isAnimating={isAnimating} />
+    </>
+  );
+};
+
+Default.argTypes = {
   isAnimating: {
+    type: { name: 'boolean', required: false },
+    defaultValue: false,
+    description: 'Number indicating if the progress bar is animating.',
     control: {
       type: 'boolean',
     },
   },
   animationDuration: {
+    type: { name: 'number', required: false },
+    defaultValue: 200,
+    description: 'Number indicating the animation duration in ms.',
     control: {
       type: 'number',
     },
   },
   incrementDuration: {
+    type: { name: 'number', required: false },
+    defaultValue: 800,
+    description:
+      'Number indicating the length of time between progress bar increments in ms.',
     control: {
       type: 'number',
     },
   },
   minimum: {
+    type: { name: 'number', required: false },
+    defaultValue: 0.08,
+    description:
+      'Number between 0 and 1 indicating the minimum value of the progress bar.',
     control: {
       type: 'number',
+      min: 0.01,
+      max: 1,
+      step: 0.1,
     },
   },
   variant: {
+    type: { name: 'string', required: false },
+    defaultValue: 'perf.green',
+    description: 'color of progressing bar',
+    options: ['perf.green', 'perf.orange', 'perf.black'],
     control: {
-      type: 'text',
+      type: 'select',
+      labels: {
+        'perf.green': 'Green',
+        'perf.orange': 'Orange',
+        'perf.black': 'Black',
+      },
     },
   },
 };
 
-ProgressBar2.args = {
-  isAnimating: true,
-  animationDuration: undefined,
-  incrementDuration: undefined,
-  minimum: undefined,
+Default.args = {
   variant: 'perf.green',
 };
