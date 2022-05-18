b6db032955e7887b45e4f12225a7ab50c8349ad2
Esen Dzhailobaev <s7b5en@gmail.com>
2022-05-18 18:01:18 +0600

    feat: controlled embedded tab (#258)

--- src/components/embedded-tabs/embedded-tabs.stories.tsx
+++ src/components/embedded-tabs/embedded-tabs.stories.tsx
@@ -1,29 +1,19 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
 import React, { useState } from 'react';
-import { Box } from 'rebass';
+import { Box, Flex } from 'rebass';
 import Labeling from '../typography/labeling';
-import EmbeddedTabs, { Props } from './EmbeddedTabs';
+import EmbeddedTabs, {
+  EmbeddedTabsProps,
+  ControlledEmbeddedTabsProps,
+} from '.';
+import Button from '../button';
+import Subtitle from '../typography/subtitle';
 
 export default {
   title: 'Quartz/EmbeddedTabs',
   component: EmbeddedTabs,
+  subcomponents: { Controlled: EmbeddedTabs.Controlled },
 } as Meta;
-
-const argTypes = {
-  tabs: {
-    required: true,
-    description: 'A list of tab items.',
-  },
-  initialTab: {
-    required: false,
-    description: 'The initial tab to be selected.',
-    default: 0,
-  },
-  onTabChange: {
-    description: 'Callback to be called when a tab is selected.',
-    required: false
-  },
-};
 
 const tabs = [
   {
@@ -41,7 +31,7 @@ const tabs = [
   },
 ];
 
-const Template: Story<Props> = (props) => {
+export const Uncontrolled: Story<EmbeddedTabsProps> = (props) => {
   const [activeTab, setActiveTab] = useState(props.initialTab ?? 0);
 
   return (
@@ -54,10 +44,51 @@ const Template: Story<Props> = (props) => {
   );
 };
 
-export const Default = Template.bind({});
+export const Controlled: Story<ControlledEmbeddedTabsProps> = (props) => {
+  const [activeTab, setActiveTab] = useState(0);
 
-Default.args = {
+  return (
+    <Box width="700px">
+      <EmbeddedTabs.Controlled
+        {...props}
+        activeTab={activeTab}
+        onTabChange={setActiveTab}
+      />
+      <Box mt={3}>
+        <Labeling bold>
+          This component is controlled. Active tab: {tabs[activeTab].title}{' '}
+        </Labeling>
+        <Flex mt={2} sx={{ gap: '10px' }}>
+          <Button onClick={() => setActiveTab(2)}>Jump to Statistics</Button>
+          <Button onClick={() => setActiveTab(1)}>Jump to Results</Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+const args = {
   tabs,
 };
 
-Default.argTypes = argTypes;
+const argTypes = {
+  tabs: {
+    required: true,
+    description: 'A list of tab items.',
+  },
+  initialTab: {
+    required: false,
+    description: 'The initial tab to be selected.',
+    default: 0,
+  },
+  onTabChange: {
+    description: 'Callback to be called when a tab is selected.',
+    required: false,
+  },
+};
+
+Uncontrolled.args = args;
+Uncontrolled.argTypes = argTypes;
+
+Controlled.args = args;
+Controlled.argTypes = argTypes;
--- src/components/embedded-tabs/index.ts
+++ src/components/embedded-tabs/index.ts
@@ -1,3 +1,14 @@
-export { default } from './EmbeddedTabs';
-export type { Props as EmbeddedTabsProps } from './EmbeddedTabs';
 export type { Props as EmbeddedTabsItem } from './Tab';
+
+import EmbeddedTabs, { Controlled } from './EmbeddedTabs';
+
+type IEmbeddedTabs = typeof EmbeddedTabs;
+
+interface EmbeddedTabsComponent extends IEmbeddedTabs {
+  Controlled: typeof Controlled;
+}
+
+(EmbeddedTabs as EmbeddedTabsComponent).Controlled = Controlled;
+
+export type { Props as EmbeddedTabsProps, ControlledProps as ControlledEmbeddedTabsProps } from './EmbeddedTabs';
+export default EmbeddedTabs as EmbeddedTabsComponent;
