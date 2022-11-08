fec0dbec9e72160436e3f7223aeac713bd94c4b4
Esen Dzhailobaev <s7b5en@gmail.com>
2022-11-08 07:17:10 -0800

    fix(datepicker): position alignment
    * fix: datepicker
    
    now it doesn't fail
    
    * fix: linter
    
    * fix: linter 2

--- src/components/datepicker/datepicker.styles.ts
+++ src/components/datepicker/datepicker.styles.ts
@@ -1,78 +1,72 @@
 import { SxStyleProp } from 'rebass';
 
-export default (align: 'left' | 'right') =>
-  ({
-    '.react-datepicker': {
-      boxShadow: 'notification',
-      borderRadius: 'initial',
-    },
+export default {
+  '.react-datepicker': {
+    boxShadow: 'notification',
+    borderRadius: 'initial',
+  },
 
-    '.react-datepicker__navigation ': {
-      outline: 'none',
-    },
+  '.react-datepicker__navigation ': {
+    outline: 'none',
+  },
 
-    '.react-datepicker-popper': {
-      top: '-25px !important',
-      left: align === 'right' ? '-270px !important' : '0',
-    },
+  '.react-datepicker__input-container input': {
+    width: '50px',
+    visibility: 'hidden',
+  },
 
-    '.react-datepicker__input-container input': {
-      width: '50px',
-      visibility: 'hidden',
-    },
+  '.react-datepicker__triangle': {
+    display: 'none',
+  },
 
-    '.react-datepicker__triangle': {
-      display: 'none',
-    },
-
-    '.react-datepicker__time-container .react-datepicker__time .react-datepicker__time-box ul.react-datepicker__time-list li.react-datepicker__time-list-item: hover':
-      {
-        backgroundColor: 'grayShade3',
-        color: 'black',
-      },
-    '.react-datepicker__time .react-datepicker__time-box ul.react-datepicker__time-list li.react-datepicker__time-list-item--selected':
-      {
-        backgroundColor: 'primary',
-        color: 'white',
-      },
-    '.react-datepicker__time .react-datepicker__time-box ul.react-datepicker__time-list li.react-datepicker__time-list-item--selected: hover':
-      {
-        backgroundColor: 'primary',
-        color: 'white',
-      },
-    '.react-datepicker__time-container .react-datepicker__time .react-datepicker__time-box ul.react-datepicker__time-list li.react-datepicker__time-list-item':
-      {
-        fontWeight: '500',
-        height: '20px',
-        lineHeight: '20px',
-      },
-
-    '.react-datepicker__day: hover': {
+  '.react-datepicker__time-container .react-datepicker__time .react-datepicker__time-box ul.react-datepicker__time-list li.react-datepicker__time-list-item: hover':
+    {
       backgroundColor: 'grayShade3',
       color: 'black',
     },
-    '.react-datepicker__day': {
-      fontWeight: 500,
-      borderRadius: 0,
-    },
-    '.react-datepicker__day--selected': {
+  '.react-datepicker__time .react-datepicker__time-box ul.react-datepicker__time-list li.react-datepicker__time-list-item--selected':
+    {
       backgroundColor: 'primary',
       color: 'white',
     },
-    '.react-datepicker__day--selected: hover': {
+  '.react-datepicker__time .react-datepicker__time-box ul.react-datepicker__time-list li.react-datepicker__time-list-item--selected: hover':
+    {
       backgroundColor: 'primary',
       color: 'white',
     },
-
-    '.react-datepicker__day: focus': {
-      outline: 'none',
+  '.react-datepicker__time-container .react-datepicker__time .react-datepicker__time-box ul.react-datepicker__time-list li.react-datepicker__time-list-item':
+    {
+      fontWeight: '500',
+      height: '20px',
+      lineHeight: '20px',
     },
 
-    '.react-datepicker__header': {
-      backgroundColor: 'grayShade3',
-    },
+  '.react-datepicker__day: hover': {
+    backgroundColor: 'grayShade3',
+    color: 'black',
+  },
+  '.react-datepicker__day': {
+    fontWeight: 500,
+    borderRadius: 0,
+  },
+  '.react-datepicker__day--selected': {
+    backgroundColor: 'primary',
+    color: 'white',
+  },
+  '.react-datepicker__day--selected: hover': {
+    backgroundColor: 'primary',
+    color: 'white',
+  },
 
-    'react-datepicker__day-name': {
-      fontWeight: 500,
-    },
-  } as SxStyleProp);
+  '.react-datepicker__day: focus': {
+    outline: 'none',
+  },
+
+  '.react-datepicker__header': {
+    backgroundColor: 'grayShade3',
+  },
+
+  'react-datepicker__day-name': {
+    fontWeight: 500,
+  },
+} as SxStyleProp;
