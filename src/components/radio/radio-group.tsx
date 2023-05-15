4080a351f31e0212558854cc1bd8c0c7abd685c7
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2023-05-15 14:36:45 +0200

    feat(radiogroup): add ability to disable radio group option
    should also be possible to pass the tooltip text in options itself, deprecated tooltipMessages

--- src/components/radio/radio-group.tsx
+++ src/components/radio/radio-group.tsx
@@ -6,16 +6,20 @@ import Radio, { RadioProps } from './index';
 // Utils
 import randomString from '../../utils/randomString';
 
-export type RadioGroupOption<T = string> = {
+export type RadioGroupOption<T = string> = RadioProps & {
   key: T;
   text: string;
-  additionalText?: string;
 };
 export interface RadioGroupProps<T = string>
   extends Omit<RadioProps, 'label' | 'onChange' | 'options' | 'value'> {
   value: T;
   options: string[] | RadioGroupOption<T>[];
   onChange: (value: T) => void;
+  /**
+   * @deprecated [#1] since version 2.6.0 [#2].
+   * [3#] will be deleted in version 3.0.
+   * [#4] In case you need similar behavior add tooltip prop through options
+   */
   tooltipMessages?: {
     [key: string]: string;
   };
@@ -51,7 +55,7 @@ const RadioGroup: <T = string>(props: RadioGroupProps<T>) => JSX.Element = ({
 
   return (
     <Flex flexDirection={flexDirection}>
-      {mappedOptions?.map(({ key, text, additionalText }) => (
+      {mappedOptions?.map(({ key, text, ...restProps }) => (
         <Radio
           {...props}
           key={`${key}`}
@@ -60,9 +64,10 @@ const RadioGroup: <T = string>(props: RadioGroupProps<T>) => JSX.Element = ({
           checked={value === key}
           label={text}
           name={name}
-          addtionalText={additionalText}
           onChange={handleChange(key)}
           tooltip={tooltipMessages[`${key}`]}
+          tooltipPosition={flexDirection === 'column' ? 'right' : 'bottom'}
+          {...restProps}
         />
       ))}
     </Flex>
