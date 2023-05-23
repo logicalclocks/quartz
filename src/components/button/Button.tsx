443a400fce3a49c39ff8de47cea7ddb8c0c37db2
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2023-05-23 17:25:47 +0200

    fix(button): to make a release

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -26,6 +26,7 @@ export interface Props extends Omit<ButtonProps, 'css'> {
   isLoading?: boolean;
   /** When `true`, and `isLoading`, doesn't show button text, only spinner */
   loadingOnly?: boolean;
+  /** guess what */
   disabled?: boolean;
 }
 
