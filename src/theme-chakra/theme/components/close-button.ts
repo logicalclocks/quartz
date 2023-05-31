c89f0c6af39498d84422edced90c1183ab2dd9f3
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2023-05-31 10:13:33 +0200

    style(button): narrows box shadows for buttons

--- src/theme-chakra/theme/components/close-button.ts
+++ src/theme-chakra/theme/components/close-button.ts
@@ -1,9 +1,9 @@
 export const closeButton = {
   borderRadius: '0.7px',
   ':focus-visible': {
-    boxShadow: '0 0 0 3px rgba(33, 177, 130, 0.6)',
+    boxShadow: '0 0 0 2px rgba(33, 177, 130, 0.6)',
     _dark: {
-      boxShadow: '0 0 0 3px rgba(33, 177, 130, 0.6)',
+      boxShadow: '0 0 0 2px rgba(33, 177, 130, 0.6)',
     },
   },
 };
