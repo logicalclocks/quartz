7021538c09578a0375b03051c96c1808fb1f687e
ehsan-github <19144304+ehsan-github@users.noreply.github.com>
2023-06-07 18:13:15 +0000

    Deploying to gh-pages from @ logicalclocks/quartz@89af85f32ba0f9c29297a880ec84ed48ebfb04cc 🚀

---
+++ assets/group.stories-754b0a0f.js
@@ -0,0 +1,10 @@
+import{j as n}from"./index.esm-d48b4efe.js";import{r as s}from"./index-8db94870.js";import{C as i}from"./checkbox-group-4a2fbaf5.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-b07c14d7.js";import"./index-0b0c7d8b.js";import"./labeling-12ecbbe1.js";import"./index-18a445f0.js";import"./index-d68b7e21.js";import"./index-a1857986.js";import"./popper-f3391c26.js";import"./chunk-QEVFQ4EU-1dcbd7c5.js";import"./index-dfb7fa5a.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-aeb30429.js";import"./index-0180fac5.js";import"./index-8ce4a492.js";const T={title:"CheckboxGroup",argTypes:{onChange:{},disabledUnselect:{control:{type:"boolean"}},label:{control:{type:"text"}},value:{}}},e={args:{label:"Validation"},render:a=>{const[p,l]=s.useState([]);return n(i,{...a,value:p,onChange:l,options:["nullable","required","email","url"]})}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    label: 'Validation'
+  },
+  render: props => {
+    const [value, setValue] = useState<string[]>([]);
+    return <CheckboxGroup {...props} value={value} onChange={setValue} options={['nullable', 'required', 'email', 'url']} />;
+  }
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const j=["GroupTemplate"];export{e as GroupTemplate,j as __namedExportsOrder,T as default};
+//# sourceMappingURL=group.stories-754b0a0f.js.map
