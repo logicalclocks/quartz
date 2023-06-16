ce1da73d5f477c616e41f1476752247392f7fa9a
ehsan-github <19144304+ehsan-github@users.noreply.github.com>
2023-06-16 08:46:59 +0000

    Deploying to gh-pages from @ logicalclocks/quartz@49f40376953649ce813653443e1bc20cc54a3991 🚀

--- utils/index.d.ts
+++ utils/index.d.ts
@@ -1,2 +1,2 @@
-export declare const copyToClipboard: (content: string) => Promise<boolean>;
+export declare const copyToClipboard: (content: string) => Promise<void>;
 export declare const saveToFile: (title?: React.ReactElement | string, content?: string) => void;
