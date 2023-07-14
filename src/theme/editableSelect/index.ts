ae4754c3d6c8d2e0ad6b10e5797d33d229dd1f41
Esen <s7b5en@gmail.com>
2023-07-14 09:56:09 +0600

    remove editableselect theme

--- src/theme/editableSelect/index.ts
+++
@@ -1,9 +0,0 @@
-import containerVariants from './container';
-import chipVariants from './chip';
-
-const variants = {
-  container: containerVariants,
-  chip: chipVariants,
-};
-
-export default variants;
