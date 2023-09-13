399e7d9df733c7b4e0216fcca26f2610f8a8c30f
ehsan-github <19144304+ehsan-github@users.noreply.github.com>
2023-09-13 17:12:50 +0000

    Deploying to gh-pages from @ logicalclocks/quartz@08c1074aa71853979f1b33a0d97ed050dda00557 🚀

---
+++ assets/ThemeProvider-12ce294a.js
@@ -0,0 +1,5 @@
+import{j as o}from"./index.esm-4cde2ecf.js";import{G as i,c as m}from"./core.browser.esm-7fc45535.js";import{T as d,d as p,a as c}from"./theme-a979b46b.js";import{r as n}from"./index-c4905b50.js";import{u as f}from"./chunk-ZJJGQIVY-49e76302.js";const t=({children:s,colorMode:e})=>{const{colorMode:a,setColorMode:r}=f();n.useEffect(()=>{e&&r(e)},[e,r]);const l=e??a;return o.jsxs(o.Fragment,{children:[o.jsx(i,{styles:m`
+          @import url('https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=swap');
+          @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@700&display=swap');
+        `}),o.jsx(d,{theme:l==="light"?p:c,children:s})]})};try{t.displayName="ThemeProvider",t.__docgenInfo={description:"",displayName:"ThemeProvider",props:{colorMode:{defaultValue:null,description:"",name:"colorMode",required:!1,type:{name:"enum",value:[{value:'"light"'},{value:'"dark"'}]}}}}}catch{}export{t as T};
+//# sourceMappingURL=ThemeProvider-12ce294a.js.map
