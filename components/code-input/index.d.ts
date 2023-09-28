fb6917742d64b98a4c926c2172a5bda10d74f44a
ehsan-github <19144304+ehsan-github@users.noreply.github.com>
2023-09-28 14:12:55 +0000

    Deploying to gh-pages from @ logicalclocks/quartz@3f5935c2d659f6a4aed41bbf10ef3cb12cac1a05 🚀

--- components/code-input/index.d.ts
+++ components/code-input/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="react" />
+import { Completion } from '@codemirror/autocomplete';
 import { ReactCodeMirrorProps } from '@uiw/react-codemirror';
 import { BoxProps } from '../box';
 import { Intents } from '../intents';
@@ -18,6 +19,7 @@ export interface CodeInputProps extends Omit<BoxProps, 'children' | 'onChange'> 
     info?: string;
     intent?: Intents;
     codeMirrorProps?: ReactCodeMirrorProps;
+    completions?: Completion[];
 }
 declare const CodeInput: import("react").ForwardRefExoticComponent<Omit<CodeInputProps, "ref"> & import("react").RefAttributes<unknown>>;
 export default CodeInput;
