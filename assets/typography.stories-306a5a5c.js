46b55eff737471c33090540834a9e45b20b4fc88
ehsan-github <19144304+ehsan-github@users.noreply.github.com>
2024-02-20 19:32:56 +0000

    Deploying to gh-pages from @ logicalclocks/quartz@e0c277eb238461193122ccdd8d7ef2e85c6b77a8 🚀

---
+++ assets/typography.stories-306a5a5c.js
@@ -0,0 +1,25 @@
+import{j as r,B as s}from"./index.esm-95acea13.js";import{V as c}from"./value-b8103e43.js";import{T as u}from"./index-6bad2ef7.js";import{I as x,H as d,e as b}from"./index-ce763bdb.js";import{L as o}from"./labeling-36656030.js";import{M as g}from"./index-9f5bc5f4.js";import"./compact-button-4641839e.js";import"./index-7d9cfca9.js";import"./index-15e90c6d.js";import"./index-5f084431.js";import"./card-secondary-cc411c7d.js";import"./index-506c8cb2.js";import"./index-ee60001d.js";import"./index-cf35c599.js";import"./index-e8a87229.js";import"./checkbox-group-fb0e304f.js";import"./index-6774f917.js";import"./index-f009891c.js";import"./index-8ee6c85d.js";import"./index-325d028e.js";import"./index-00b1c738.js";import"./theme-c1401d61.js";import"./GetIcon-c5f7f453.js";import"./index-14281ba5.js";import"./index-76d6605f.js";import"./index-8e4f4f79.js";import"./index-f842def2.js";import"./index-2a26cb72.js";import"./index-3777f79f.js";import"./index-280deb0d.js";import"./index-c5a183b8.js";import"./index-2ffa3e6a.js";import"./index-f2badc06.js";import{T as f}from"./index-c40f68ee.js";import"./index-6a2ce10b.js";import"./slider-c0f7996d.js";import"./range-slider-dd89aad3.js";import"./index-dc620495.js";import"./index-524518b1.js";import"./index-f0cdfdb8.js";import"./index-71f4177e.js";import"./index-02a42f1e.js";import"./ThemeProvider-12f6ca24.js";import"./index-32e1c7a4.js";import"./index-c8aa8642.js";import"./index-5d579de3.js";import"./Bar-24450f1b.js";import"./index-cb56fc43.js";import"./Flex-37b3be28.js";import"./index-01e8c9da.js";import"./Dot-b5fb4070.js";import"./Badge-6b768281.js";import"./TextValueBadge-961335b2.js";import"./ProjectBadge-3771a0cd.js";import"./index-6a2b5278.js";import"./_commonjsHelpers-de833af9.js";import"./index-6e4dee96.js";import"./type-af5339dc.js";import"./_curry2-9db85ff5.js";import"./_isString-966efe80.js";import"./_isObject-11124c2f.js";import"./mergeDeepRight-032f9ec4.js";import"./mergeDeepWithKey-ac52baf9.js";import"./_curry3-2506b18d.js";import"./list-9cfea01e.js";import"./positions-4b0ff717.js";import"./index-17f883e0.js";import"./Button-a764f177.js";import"./index-093317e9.js";import"./index-8bf7f4ad.js";import"./Tab-b306e720.js";import"./AnimatedProgress-ef2f9b88.js";import"./extends-98964cd2.js";import"./FreshnessBar-3921f336.js";import"./index-34c655b6.js";import"./clamp-5454dae8.js";import"./chunk-ZJJGQIVY-9cf857b2.js";import"./index-ffe84488.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-95ca0d59.js";import"./index-2a89f1ca.js";import"./chunk-5PH6ULNP-08a08ebd.js";import"./chunk-37N6GCLA-ba2a618c.js";import"./index-0bcfbc70.js";import"./chunk-3XANSPY5-cf37bc50.js";import"./index-34ae0f6f.js";import"./index-e67c4366.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./index-4d904bdf.js";import"./index-902fb94b.js";import"./FileItemExplorer-b1789f8f.js";import"./index-edcf16e4.js";import"./iframe-7ce9b4b5.js";import"../sb-preview/runtime.js";import"./nth-b0f2fe30.js";import"./popper-f3391c26.js";import"./index-948712dc.js";import"./isNativeReflectConstruct-22f77b05.js";import"./setPrototypeOf-0bb37fbe.js";import"./index-d7567f6d.js";import"./inheritsLoose-d541526f.js";import"./core.browser.esm-1eea21ee.js";const ui={title:"Typography"},h=()=>r.jsxs(s,{sx:{display:"flex",flexDirection:"column","> *":{lineHeight:"30px"}},children:[r.jsx(u,{uppercase:!0,children:"Titrage/H1"}),r.jsx(f,{children:"Text"}),r.jsx(d,{children:"hoverable"}),r.jsx(b,{children:"Titrage/H2"}),r.jsx(o,{bold:!0,children:"labeur/bold"}),r.jsx(o,{children:"labeur/regular"}),r.jsx(o,{gray:!0,children:"labeur/gray"}),r.jsx(c,{children:"value/default"}),r.jsx(g,{children:"labeling/micro"})]}),i=()=>r.jsx(s,{sx:{display:"flex",flexDirection:"column","> *":{lineHeight:"30px"}},children:r.jsx(x,{intent:"success",children:"the sum is 100%"})}),t=h.bind({});var e,p,m;i.parameters={...i.parameters,docs:{...(e=i.parameters)==null?void 0:e.docs,source:{originalSource:`() => <Box sx={{
+  display: 'flex',
+  flexDirection: 'column',
+  '> *': {
+    lineHeight: '30px'
+  }
+}}>
+    <InputValidation intent="success">the sum is 100%</InputValidation>
+  </Box>`,...(m=(p=i.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var l,a,n;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`() => <Box sx={{
+  display: 'flex',
+  flexDirection: 'column',
+  '> *': {
+    lineHeight: '30px'
+  }
+}}>
+    <Title uppercase>Titrage/H1</Title>
+    <Text>Text</Text>
+    <HoverableText>hoverable</HoverableText>
+    <Subtitle>Titrage/H2</Subtitle>
+    <Labeling bold>labeur/bold</Labeling>
+    <Labeling>labeur/regular</Labeling>
+    <Labeling gray>labeur/gray</Labeling>
+    <Value>value/default</Value>
+    <Microlabeling>labeling/micro</Microlabeling>
+  </Box>`,...(n=(a=t.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const xi=["DefaultInfo","Default"];export{t as Default,i as DefaultInfo,xi as __namedExportsOrder,ui as default};
